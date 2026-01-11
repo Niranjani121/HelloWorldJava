a47e1dd5effe428a8f1d2862f82d2a0de1047ca4
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-10 23:36:18 -0800

    Changes in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("helloo");
+        int a=10;
+        int b=3;
+        System.out.println("Addition"+(a+b));
+        System.out.println("Subtraction"+(a-b));
+        System.out.println("Multiplication"+(a*b));
+        System.out.println("Modulation"+(a/b));
+        System.out.println("helloo , This is Ranjani");
     }
 }
