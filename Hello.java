e1390fe3df8bf9c124c4b842782af368c8056782
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-13 15:40:23 -0800

    Hello.java file modified

--- Hello.java
+++ Hello.java
@@ -9,17 +9,17 @@ public class Hello{
         boolean isLesser=(a<b);
         boolean isGreaterOrEqual=(a>=b);
         boolean isLesserOrEqual=(a<=b);
-        
+
         System.out.println("Addition value"+ (a+b));
         System.out.println("Subtraction value is"+ (a-b));
         System.out.println("Multiplication value is"+ (a*b));
         System.out.println("Modulation value is"+ (a/b));
-        System.out.println("a==b" +isEqual);
-        System.out.println("a!=b" +isNotEqual);
-        System.out.println("a>b" +isGreater);
-        System.out.println("a<b" +isLesser);
-        System.out.println("a>=b" +isGreaterOrEqual);
-        System.out.println("a<=b" +isLesserOrEqual);
+        System.out.println("a==b" + isEqual);
+        System.out.println("a!=b" + isNotEqual);
+        System.out.println("a>b" + isGreater);
+        System.out.println("a<b" + isLesser);
+        System.out.println("a>=b" + isGreaterOrEqual);
+        System.out.println("a<=b" + isLesserOrEqual);
 
         System.out.println("Helloo , This is Ranjani program");
     }
