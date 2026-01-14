7c92742b07fd80a6cc32e7c8a2b076795ccb814e
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-13 19:16:41 -0800

    Oper.java file added

---
+++ Oper.java
@@ -0,0 +1,15 @@
+public class Oper {
+   public Oper() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "admin";
+      String var2 = "1234";
+      if (var1.equals("admin") && var2.equals("password")) {
+         System.out.println("Login successfully");
+      } else {
+         System.out.println("Invalid username and password");
+      }
+
+   }
+}
