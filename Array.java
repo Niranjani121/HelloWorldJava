5589ddaeeda3acbe1f64d3caea0d31aa8c61873f
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-14 13:47:37 -0800

    Array.java file modified

--- Array.java
+++ Array.java
@@ -1,8 +1,10 @@
 public class Array{
     public static void main(String[] args) {
         int[] numbers={1,2,3,4,5,6};
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
-        System.out.println(numbers[3]);
+        int length=numbers.length;
+        System.out.println("The length of the array is"+length);
+        System.out.println(numbers[4]);
+        System.out.println(numbers[5]);
+        System.out.println(numbers[6]);
     } 
 }
