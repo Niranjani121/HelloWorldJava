de0200d0377422e09cd141d96ce560cdd47328b2
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-14 14:12:08 -0800

    String.java file created

---
+++ String.java
@@ -0,0 +1,8 @@
+public class String{
+    public static void main(String[] args){
+        String str1="Hello";
+        String str2="world";
+        String result="Java";
+        System.out.println("Concatenation :"+str1+str2+result);
+    }
+}
