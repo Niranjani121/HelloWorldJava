bdbdca674bd0689055bbb2b9d357b8bc738de46b
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-14 14:33:06 -0800

    StringExam.java file added

---
+++ StringExam.java
@@ -0,0 +1,8 @@
+public class StringExam{
+    public static void main(String[] args){
+        String str1=Hello;
+        String str2=world;
+        String result=Java;
+        System.out.println("Concatenation :"+str1+str2+result);
+    }
+}
