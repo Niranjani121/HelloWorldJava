0c6fabc560fc64432481b629a8a90e287918a261
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-17 12:49:20 -0800

    Modified file Oper.java

--- Oper.java
+++ Oper.java
@@ -5,6 +5,15 @@ public class Oper {
    public static void main(String[] var0) {
       String var1 = "admin";
       String var2 = "1234";
+      boolean isdoorOpen=false;
+
+      if(!isdoorOpen){
+         System.out.println("Door closed");
+      }
+      else{
+         System.out.println("Door closed");
+      }
+
       if (var1.equals("admin") && var2.equals("password")) {
          System.out.println("Login successfully");
       } else {
